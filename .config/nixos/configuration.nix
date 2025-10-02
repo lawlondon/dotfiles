18eeb01d35198877065f4e51f0d1526ecc875cae
London W <londonalexanderw@gmail.com>
2025-10-02 11:54:42 +0800

    lua

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -12,6 +12,7 @@
       ./modules/gaming.nix
       ./modules/packages.nix
       ./modules/shell.nix
+      ./modules/programming.nix
     ];
 
   system.stateVersion = "25.05"; # Don't ever touch this option. Defines the intial version Nix was installed at.
